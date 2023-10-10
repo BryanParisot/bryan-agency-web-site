19d92cae968d190480f54632976c6fb41d747c0b
Bryan <bryanparisot@hotmail.com>
2023-10-10 12:30:25 +0200

    feat(button): create button component

---
+++ components/ui/button.stories.jsx
@@ -0,0 +1,8 @@
+import { Button } from "./button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+};
+
+export const ButtonGeneral = () => <Button variant="secondary"  size="lg">Contactez-nous </Button>;
