bd54f2d6e382437b10f4d58fdfde01e53f7f8e20
Bryan <bryanparisot@hotmail.com>
2023-10-10 11:14:19 +0200

    feat(nav): create nav et add story

--- components/Button.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "./ui/button"
-
-export default {
-    title: "Button",
-    component: Button,
-};
-
-export const ButtonHome = () => <Button size="lg" variant="outline">Hello</Button>
