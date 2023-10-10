21d6d446863bcd74c1425db6d3f864edb4812464
Bryan <bryanparisot@hotmail.com>
2023-10-10 12:26:06 +0200

    fix(nav) : switch nav to menu components

---
+++ components/Menu/Menu.stories.jsx
@@ -0,0 +1,8 @@
+import Menu from "./Menu";
+
+export default {
+  title: "Navigation/menu",
+  component: Menu,
+};
+
+export const PrincipalMenu = () => <Menu />;
--- components/Nav/Navigation.stories.jsx
+++
@@ -1,8 +0,0 @@
-import  Navigation  from "./Navigation";
-
-export default {
-  title: "Navigation",
-  component: Navigation,
-};
-
-export const Firstnavigation = () => <Navigation />;
