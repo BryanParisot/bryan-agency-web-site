f294c11f5ca7824105f15cac6aca9372d36fd9b5
Bryan <bryanparisot@hotmail.com>
2023-10-11 14:20:45 +0200

    feat(tailwind): config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,17 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary: '#064e3b',
+      secondary: '#059669',
+      light: '#ecfdf5', 
+      sombre : '#022c22',
+      middle : '#34d399'
     },
   },
   plugins: [],
 }
-export default config
