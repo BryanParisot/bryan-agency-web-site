522773bf2414c10221c27b94e5bf835483853d2c
Bryan <bryanparisot@hotmail.com>
2023-10-23 17:59:22 +0200

    fix(homecard): " to ' , to ;

--- components/HomeCards/HomeCards.tsx
+++ components/HomeCards/HomeCards.tsx
@@ -4,18 +4,18 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 interface HomeCardsProps {
-    title: string,
-    text: string,
-    image: string,
-    alt: string,
-    link: string
-    bg: string,
+    title: string;
+    text: string;
+    image: string;
+    alt: string;
+    link: string;
+    bg: string;
 }
 
 const HomeCards: React.FC<HomeCardsProps> = ({ title, text, image, alt, link, bg }) => {
     return (
         <div className='bg-emerald-50 rounded-md w-[395px] p-5'>
-            <div className="flex flex-col !justify-between  space-y-4 h-full">
+            <div className='flex flex-col !justify-between  space-y-4 h-full'>
 
                 <div className={`${bg} flex items-center justify-end w-20 h-20 rounded p-3`}>
                     <Image src={image} alt={alt} width={100} height={100} />
@@ -25,14 +25,14 @@ const HomeCards: React.FC<HomeCardsProps> = ({ title, text, image, alt, link, bg
                     <h3 className='text-emerald-950 font-extrabold text-lg'>
                         {title}
                     </h3>
-                    <p className='text-gray-700 font-light '>
+                    <p className='text-gray-700 font-light'>
                         {text}
                     </p>
                 </div>
 
                 <div className='flex justify-end'>
                     <Link href={link}>
-                        <Button variant="outline" size="lg">
+                        <Button variant='outline' size='lg'>
                             En savoir plus
                         </Button>
                     </Link>
