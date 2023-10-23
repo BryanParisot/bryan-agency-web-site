e5701126224e0a6257da9d0b2658d8c638060be6
Bryan <bryanparisot@hotmail.com>
2023-10-23 18:03:37 +0200

    fix(header): indentation

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 
 
 interface HeaderProps {
-    title: string,
-    subtitle: string,
-    text: string
-    image: string,
-    alt: string
+    title: string;
+    subtitle: string;
+    text: string;
+    image: string;
+    alt: string;
 }
 
 
@@ -18,11 +18,19 @@ const Header: React.FC<HeaderProps> = ({ title, subtitle, text, image, alt }) =>
             <div className='mt-24 sm:mt-46 flex flex-row items-center justify-between px-12 sm:px-20'>
                 <div className='sm:w-3/6 w-full space-y-4'>
                     <div className='space-y-3 mb-5'>
-                        <h1 className='text-base sm:text-2xl font-bold text-emerald-400'>{title}</h1>
-                        <h2 className='font-semibold text-emerald-50 text-2xl sm:text-3xl'>{subtitle}</h2>
-                        <p className='text-emerald-50 sm:text-lg text-base'>{text}</p>
+                        <h1 className='text-base sm:text-2xl font-bold text-emerald-400'>
+                            {title}
+                        </h1>
+                        <h2 className='font-semibold text-emerald-50 text-2xl sm:text-3xl'>
+                            {subtitle}
+                        </h2>
+                        <p className='text-emerald-50 sm:text-lg text-base'>
+                            {text}
+                        </p>
                     </div>
-                    <Button>Discutons de votre projet</Button>
+                    <Button>
+                        Discutons de votre projet
+                    </Button>
                 </div>
                 <div className='hidden  sm:block :w-2/6'>
                     <Image src={image} alt={alt} />
