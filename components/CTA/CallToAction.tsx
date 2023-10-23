e6ea92a2c6096118cbb04de1b52ee2cf73094f4d
Bryan <bryanparisot@hotmail.com>
2023-10-23 15:32:01 +0200

    fix(cta/navigation): modified z-index

--- components/CTA/CallToAction.tsx
+++ components/CTA/CallToAction.tsx
@@ -10,7 +10,7 @@ function CallToAction() {
                 <div className="relative isolate overflow-hidden bg-emerald-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
                     <svg
                         viewBox="0 0 1024 1024"
-                        className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+                        className="absolute left-1/2 top-1/2 -z-1 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
                         aria-hidden="true"
                     >
                         <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
@@ -28,15 +28,15 @@ function CallToAction() {
                             Contactez-nous dès aujourd'hui pour commencer votre voyage vers le succès.
                         </h2>
                         <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                            <a
+                            <Link
                                 href="#"
                                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                             >
                                 Get started
-                            </a>
-                            <a href="#" className="text-sm font-semibold leading-6 text-white">
+                            </Link>
+                            <Link href="#" className="text-sm font-semibold leading-6 text-white">
                                 Learn more <span aria-hidden="true">→</span>
-                            </a>
+                            </Link>
                         </div>
                     </div>
                     <div className="relative mt-16 h-80 lg:mt-8">
--- components/Nav/Navigation.tsx
+++ components/Nav/Navigation.tsx
@@ -11,7 +11,7 @@ export default function Navigation() {
 
     return (
         <header className='flex justify-center'>
-            <div className='fixed shadow flex items-center justify-between bg-emerald-50/90 h-20 w-full max-w-[90%] rounded-lg text mt-10 px-5'>
+            <div className='fixed shadow flex items-center justify-between bg-emerald-50/90 h-20 w-full max-w-[90%] rounded-lg text mt-10 px-5 z-10'>
                 <div>Logo Here</div>
                 <div className='w-96 hidden sm:block'><Menu /></div>
                 <Link className='hidden sm:block' href="/contact"> <Button variant="secondary" size="lg">Contactez-nous</Button></Link>
