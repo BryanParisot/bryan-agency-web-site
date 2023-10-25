00203673e37ad54cdd62b01c63f15fc7b77d33e9
Bryan <bryanparisot@hotmail.com>
2023-10-25 12:38:45 +0200

    feat(section): create section logos

---
+++ components/Section/SectionLogos.stories.jsx
@@ -0,0 +1,10 @@
+import SectionLogos from './SectionLogos';
+
+export default {
+  title: 'Components/SectionLogo',
+  component: SectionLogos,
+};
+
+export const Logos = () => (
+  <SectionLogos title='Exemple de fonctionnalités que vous pouvez ajouter à votre site vitrine' />
+);
---
+++ components/Section/SectionLogos.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+//LOGOS
+import logo_booking from '../../public/logos/site/vitrine/logo_booking.svg'
+import logo_chat from '../../public/logos/site/vitrine/logo_chat.svg'
+import logo_form from '../../public/logos/site/vitrine/logo_form.svg'
+import logo_translation from '../../public/logos/site/vitrine/logo_translation.svg'
+import Image from 'next/image'
+
+interface SectionLogosProps {
+    title: string;
+}
+
+const SectionLogos: React.FC<SectionLogosProps> = ({ title }) => {
+
+    const dataSection = [
+        {
+            text: 'Reservation', image: logo_booking, alt: 'Logo de résérvation'
+        },
+        {
+            text: 'Messagerie', image: logo_chat, alt: 'logo messagerie'
+        },
+        {
+            text: 'Formulaire', image: logo_form, alt: 'logo formulaire'
+        },
+        {
+            text: 'Multi-langue', image: logo_translation, alt: 'logo multi langue'
+        }
+    ]
+
+    return (
+        <div className='my-20'>
+            <h2 className='text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                {title}
+            </h2>
+            <div>
+                <div className="mt-20 grid grid-cols-1 sm:grid-cols-4 gap-10 justify-items-center px-5 sm:px-20">
+                    {
+                        dataSection.map((item) => (
+                            <div key={item.text} className='flex flex-col items-center justify-center space-y-4'>
+                                <Image width={70} height={70} src={item.image} alt={item.alt} />
+                                <h3 className='text-emerald-950 text-lg font-bold'>{item.text}</h3>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SectionLogos
