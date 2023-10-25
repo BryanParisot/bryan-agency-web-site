3369aa43cf57f78825dcc1ff59ab15f6c9f788ad
Bryan <bryanparisot@hotmail.com>
2023-10-25 14:32:31 +0200

    feat(e-commerce): create page and fix typos

--- app/site/e-commerce/page.tsx
+++ app/site/e-commerce/page.tsx
@@ -1,7 +1,134 @@
 import React from 'react'
+import Image from 'next/image'
+
+//COMPONENTS
+import Header from '@/components/Header/Header'
+import CardGeneral from '@/components/Cards/CardGeneral'
+import CallToAction from '@/components/CTA/CallToAction'
+import SectionLogos from '@/components/Section/SectionLogos'
+import BlogSection from '@/components/BlogSection/BlogSection'
+import Faq from '@/components/FAQ/Faq'
+
+
+//LOGOS | PICTURES
+import picture_site from "../../../public/pictures/site/site_page.png"
+import logo_visibilite from "../../../public/logos/site/vitrine/logo_visibilite.svg"
+import logo_promotion from "../../../public/logos/site/vitrine/logo_promotion.svg"
+import logo_roi from "../../../public/logos/site/vitrine/logo_roi.svg"
+import double_screen from "../../../public/pictures/site/vitrine/double_screen.png"
+import vitrine_site from "../../../public/pictures/site/vitrine/vitrine_site.png"
+
+
 
 export default function page() {
+
+  const vitrineData = [
+    {
+      title: 'Expansion des marchés et de la clientèle', text: 'Un site e-commerce permet à votre entreprise d\'avoir une présence en ligne 24 heures sur 24, 7 jours sur 7. Il est accessible aux clients potentiels à tout moment, ce qui augmente votre visibilité et votre accessibilité.', image: logo_visibilite, alt: 'Logo visibilité en ligne'
+    },
+    {
+      title: 'Réduction des coûts d\'exploitation', text: 'Un site e-commerce bien conçu permet de présenter votre entreprise, vos produits ou services, et votre identité de marque de manière professionnelle. Il offre une plateforme pour communiquer votre message et renforcer la confiance.', image: logo_promotion, alt: 'Logo promotion de votre marque'
+    },
+    {
+      title: 'Chiffre d\'affaires', text: 'Un site internet e-commerce bien conçu peut contribuer à l\'augmentation de votre chiffre d\'affaires en attirant de nouveaux clients, en augmentant la notoriété de votre entreprise et en facilitant la vente de vos produits.', image: logo_roi, alt: 'logo roi'
+    }
+  ]
+
+  const faqs = [
+    {
+      question: "Combien coûte la création d'un site e-commerce ?",
+      answer:
+        "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    },
+    {
+      question: "Quel est l’objectif d’un site internet e-commerce ?",
+      answer:
+        "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    },
+    {
+      question: "De quelles étapes ai-je besoin pour créer un site e-commerce de A à Z ?",
+      answer:
+        "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    },
+    {
+      question: "Quelle plateforme de création de site e-commerce dois-je utiliser (WordPress, Shopify, etc.) ?",
+      answer:
+        "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    },
+    // More questions...
+  ]
   return (
-    <div>page e-commerce</div>
+    <main>
+      <Header
+        title="Agence de création de e-commerce"
+        subtitle="Création d'un site internet e-commerce"
+        text="Découvrez le potentiel infini de la vente en ligne avec [Nom de Votre Entreprise]. Notre expertise en création de sites e-commerce vous permet de transformer vos idées en une boutique en ligne florissante. Offrez à vos clients une expérience d'achat en ligne exceptionnelle et développez votre entreprise comme jamais auparavant."
+        image={picture_site}
+        alt="image png developpeur web"
+      />
+
+      <div className="mt-10 mb-10">
+        <h2 className="text-center text-2xl sm:text-4xl text-emerald-950 font-bold" >
+          Les avantages de créer <span className="text-emerald-600">un site internet e-commerce </span>
+        </h2>
+
+        <div className="mt-20 grid grid-cols-1 sm:grid-cols-3 gap-10 justify-items-center px-5 sm:px-20">
+          {
+            vitrineData.map((item) => (
+              <CardGeneral key={item.title} title={item.title} text={item.text} image={item.image} alt={item.alt} />
+            ))
+          }
+        </div>
+
+        <div className='flex flex-col sm:flex-row mt-20 sm:max-h-[500px]  h-full'>
+          <div className="flex items-center sm:order-first sm:w-1/2 md:w-6/12">
+            <Image className="w-full h-full" src={double_screen} alt="Double Ecran illustration" />
+          </div>
+          <div className='w-full sm:w-3/6 space-y-4 px-8'>
+            <h2 className="mt-2 text-2xl font-bold sm:text-left md:text-4xl text-emerald-950">
+              Transformez votre entreprise en ligne avec une boutique e-commerce sur mesure
+            </h2>
+            <p>
+              Chez [Nom de Votre Entreprise], nous sommes passionnés par la création de boutiques en ligne qui se démarquent. Notre expertise en développement e-commerce vous permet de créer une expérience d'achat en ligne exceptionnelle pour vos clients. Que vous vendiez des produits physiques, des services ou des produits numériques, nous concevons des sites e-commerce sur mesure qui répondent à vos besoins spécifiques.
+            </p>
+            <p>
+              Nous comprenons l'importance de la convivialité, de la sécurité et de la performance pour votre boutique en ligne. C'est pourquoi nous mettons tout en œuvre pour vous offrir une plateforme e-commerce qui maximise vos ventes et fidélise vos clients.
+            </p>
+            <p>
+              Explorez nos services de création de sites e-commerce et découvrez comment nous pouvons vous aider à développer votre entreprise en ligne. De la conception à la gestion, nous sommes là à chaque étape de votre parcours e-commerce.
+            </p>
+          </div>
+        </div>
+
+        <div className='flex flex-col sm:flex-row sm:max-h-[500px] h-full mt-10 sm:mt-0'>
+          <div className="mb-8 flex items-center sm:order-last sm:w-1/2 md:w-6/12">
+            <Image className="w-full h-full" src={vitrine_site} alt="Illustration d'un site vitrine" />
+          </div>
+          <div className='w-full sm:w-3/6 space-y-4 px-8 mt-5'>
+            <h2 className="mt-2 text-2xl font-bold sm:text-left md:text-4xl text-emerald-950">
+              Un Site internet e-commerce qui vous ressemble
+            </h2>
+            <p>
+              Votre boutique en ligne est plus qu'un simple site web, c'est le visage de votre entreprise sur internet. Chez [Nom de Votre Entreprise], nous comprenons l'importance de créer une boutique en ligne qui reflète votre identité de marque et votre vision.
+            </p>
+            <p>
+              Que vous soyez une petite entreprise artisanale ou une entreprise établie, nous travaillons en étroite collaboration avec vous pour concevoir une boutique en ligne qui vous ressemble. De la personnalisation du design à l'intégration des fonctionnalités spécifiques, nous mettons tout en œuvre pour capturer l'essence de votre entreprise.
+            </p>
+            <p>
+              Votre site e-commerce devrait être unique, tout comme vous. Faites confiance à notre équipe d'experts en développement e-commerce pour créer une boutique en ligne exceptionnelle qui séduit vos clients et vous aide à réussir dans le monde de la vente en ligne. Découvrez comment nous pouvons faire de votre boutique en ligne une extension authentique de votre entreprise.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='sm:px-20 px-5'>
+        <CallToAction />
+      </div>
+      <SectionLogos title="Exemple de fonctionnalités que vous pouvez ajouter à votre site e-commerce" />
+      <BlogSection />
+      <Faq faq={faqs} />
+
+
+
+    </main>
   )
 }
--- app/site/vitrine/page.tsx
+++ app/site/vitrine/page.tsx
@@ -12,7 +12,7 @@ import { Button } from '@/components/ui/button'
 import SectionEmerald from '@/components/Section/SectionEmerald'
 
 
-//LOGOS
+//LOGOS | PICTURES
 import picture_site from "../../../public/pictures/site/site_page.png"
 import logo_visibilite from "../../../public/logos/site/vitrine/logo_visibilite.svg"
 import logo_promotion from "../../../public/logos/site/vitrine/logo_promotion.svg"
@@ -27,13 +27,13 @@ export default function page() {
 
   const vitrineData = [
     {
-      title: 'Visibilité en Ligne', text: 'Un site vitrine permet à votre entreprise d\'avoir une présence en ligne 24 heures sur 24, 7 jours sur 7. Il est accessible aux clients potentiels à tout moment, ce qui augmente votre visibilité et votre accessibilité.', image: logo_visibilite, alt: 'Logo visibilité en ligne'
+      title: 'Visibilité en ligne', text: 'Un site vitrine permet à votre entreprise d\'avoir une présence en ligne 24 heures sur 24, 7 jours sur 7. Il est accessible aux clients potentiels à tout moment, ce qui augmente votre visibilité et votre accessibilité.', image: logo_visibilite, alt: 'Logo visibilité en ligne'
     },
     {
-      title: 'Promotion de Votre Marque', text: 'Un site vitrine bien conçu permet de présenter votre entreprise, vos produits ou services, et votre identité de marque de manière professionnelle. Il offre une plateforme pour communiquer votre message et renforcer la confiance.', image: logo_promotion, alt: 'Logo promotion de votre marque'
+      title: 'Promotion de votre marque', text: 'Un site vitrine bien conçu permet de présenter votre entreprise, vos produits ou services, et votre identité de marque de manière professionnelle. Il offre une plateforme pour communiquer votre message et renforcer la confiance.', image: logo_promotion, alt: 'Logo promotion de votre marque'
     },
     {
-      title: 'Chiffre d\'Affaires', text: 'Un site vitrine bien conçu peut contribuer à l\'augmentation de votre chiffre d\'affaires en attirant de nouveaux clients, en augmentant la notoriété de votre entreprise et en facilitant la vente de vos produits ou services en ligne.', image: logo_roi, alt: 'logo roi'
+      title: 'Chiffre d\'affaires', text: 'Un site vitrine bien conçu peut contribuer à l\'augmentation de votre chiffre d\'affaires en attirant de nouveaux clients, en augmentant la notoriété de votre entreprise et en facilitant la vente de vos produits ou services en ligne.', image: logo_roi, alt: 'logo roi'
     }
   ]
 
@@ -66,7 +66,7 @@ export default function page() {
     <main>
       <Header
         title="Agence de création de site vitrine"
-        subtitle="Création d'un Site Vitrine Sur Mesure"
+        subtitle="Création d'un site vitrine sur mesure"
         text="Découvrez l'élégance et la simplicité d'un site vitrine conçu spécialement pour votre entreprise. Chez [Nom de Votre Entreprise], nous mettons en avant votre image de marque et vos produits/services de manière captivante. Transformez les visiteurs en clients avec un site web vitrine qui laisse une impression mémorable."
         image={picture_site}
         alt="image png developpeur web"
@@ -91,7 +91,7 @@ export default function page() {
           </div>
           <div className='w-full sm:w-3/6 space-y-4 px-8'>
             <h2 className="mt-2 text-2xl font-bold sm:text-left md:text-4xl text-emerald-950">
-              Boostez Votre Entreprise à Nancy avec Notre Expertise en Création de Sites vitrine
+              Boostez votre entreprise à Nancy avec notre expertise en création de sites vitrine
             </h2>
             <p>
               Vous êtes basé à Nancy ou dans ses environs et vous cherchez à développer votre entreprise en ligne ? Ne cherchez pas plus loin. Chez [Nom de Votre Entreprise], nous sommes fiers d'offrir nos services de création de sites web sur mesure spécialement conçus pour les entreprises locales de Nancy.
