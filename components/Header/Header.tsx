a58d832a55024b3170c6ad294ec6c041889e88fc
Bryan <bryanparisot@hotmail.com>
2023-11-05 13:34:50 +0100

    fix(header): Component improvement

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import Image, { StaticImageData } from 'next/image'
+import { ChatBubbleBottomCenterIcon } from '@heroicons/react/24/outline'
 
 
 interface HeaderProps {
@@ -18,17 +19,19 @@ const Header: React.FC<HeaderProps> = ({ title, subtitle, text, image, alt }) =>
             <div className='mt-24 sm:mt-46 flex flex-row items-center justify-between px-12 sm:px-20'>
                 <div className='sm:w-3/6 w-full space-y-4'>
                     <div className='space-y-3 mb-5'>
-                        <h1 className='text-base sm:text-2xl font-bold text-emerald-400'>
+                        <h1 className='text-base sm:text-3xl font-bold text-emerald-400'>
                             {title}
                         </h1>
-                        <h2 className='font-semibold text-emerald-50 text-2xl sm:text-3xl'>
+                        <h2 className='font-semibold text-emerald-50 text-2xl sm:text-4xl'>
                             {subtitle}
                         </h2>
                         <p className='text-emerald-50 sm:text-lg text-base'>
                             {text}
                         </p>
                     </div>
+
                     <Button>
+                        <ChatBubbleBottomCenterIcon className="h-5 w-5 mr-2" aria-hidden="true" />
                         Discutons de votre projet
                     </Button>
                 </div>
