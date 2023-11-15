e40e71c54dc9331a03ab731b573e3ccc74336108
Bryan <bryanparisot@hotmail.com>
2023-11-15 12:53:48 +0100

    feat(form): update form for ux

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,34 +1,46 @@
 "use client"
 
 import { sendEmail } from '@/utils/send-email';
-import React, { FC } from 'react'
-
-
+import React, { FC, useState } from 'react'
+import { Toaster, toast } from 'react-hot-toast';
 import { useForm } from 'react-hook-form';
+import { Button } from '../ui/button';
 
 export type FormData = {
     prenom: string;
     nom: string;
     email: string;
     message: string;
-    numero: string;
+    numero: number;
 };
 
 const Form: FC = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
-    const onSubmitHandler = (data: FormData) => {
-        sendEmail(data);
-    }
+    const [loading, setLoading] = useState(false);
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>();
+
+    const onSubmitHandler = async (data: FormData) => {
+        setLoading(true);
+        try {
+            await sendEmail(data);
+            toast.success('Email envoyé avec succès!');
+            reset()
+        } catch (error) {
+            toast.error('Erreur lors de l\'envoi de l\'email');
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div>
+            <Toaster />
             <form onSubmit={handleSubmit(onSubmitHandler)} className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
                 <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
                     <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                         <div>
                             <label htmlFor="prenom" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Prenom
+                                Prenom *
                             </label>
                             <div className="mt-2.5">
                                 <input
@@ -42,7 +54,7 @@ const Form: FC = () => {
                         </div>
                         <div>
                             <label htmlFor="nom" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Nom
+                                Nom *
                             </label>
                             <div className="mt-2.5">
                                 <input
@@ -57,7 +69,7 @@ const Form: FC = () => {
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Email
+                                Email *
                             </label>
                             <div className="mt-2.5">
                                 <input
@@ -72,11 +84,11 @@ const Form: FC = () => {
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="numero" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Numéro
+                                Numéro *
                             </label>
                             <div className="mt-2.5">
                                 <input
-                                    type="tel"
+                                    type="number"
                                     id="numero"
                                     autoComplete="tel"
                                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -86,7 +98,7 @@ const Form: FC = () => {
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Message
+                                Message *
                             </label>
                             <div className="mt-2.5">
                                 <textarea
@@ -101,12 +113,14 @@ const Form: FC = () => {
                         </div>
                     </div>
                     <div className="mt-8 flex justify-end">
-                        <button
+                        <Button
+                            variant="primary"
+                            size="lg"
                             type="submit"
-                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            disabled={loading}
                         >
-                            Envoyer
-                        </button>
+                            {loading ? 'Envoi en cours...' : 'Envoyer'}
+                        </Button>
                     </div>
                 </div>
             </form>
@@ -115,4 +129,4 @@ const Form: FC = () => {
     )
 }
 
-export default Form
+export default Form                          
