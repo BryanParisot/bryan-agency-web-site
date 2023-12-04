c17cb32c4831c884376a711fe1d0451776c7028f
Bryan <bryanparisot@hotmail.com>
2023-12-04 20:23:37 +0100

    fix(sitemap): string

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -24,7 +24,7 @@ const changeFrequency: SitemapEntry['changeFrequency'] = 'daily';
 
     const post = posts.map(({url, date}) => ({
       url: `${siteUrl}${url}`,
-      lastModified: date.toString(),
+      lastModified: date,
       changeFrequency
     }))
 
