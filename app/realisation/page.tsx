f8b316acd32b272c4df0c84e83ce07c83b2bd84e
Bryan <bryanparisot@hotmail.com>
2023-12-09 19:11:57 +0100

    fix(realisation): class to className

--- app/realisation/page.tsx
+++ app/realisation/page.tsx
@@ -18,7 +18,7 @@ export default function page() {
   return (
     <div className='flex flex-col justify-center items-center mt-40'>
       <h2 className='mb-10 text-6xl font-bold text-emerald-950'>En cours de construction... </h2>
-      <iframe src="https://giphy.com/embed/3oKIPnAiaMCws8nOsE" width="457" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      <iframe src="https://giphy.com/embed/3oKIPnAiaMCws8nOsE" width="457" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
     </div>
   )
 }
